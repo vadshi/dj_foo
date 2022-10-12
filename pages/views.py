d1e779aed808ab98f5ed9cf357efa4451a50d477
vadshi <vadimx30@gmail.com>
2022-10-12 14:33:25 +0300

    Add CBVs and tests

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 # Create your views here.
-def home_page_view(request):
-    return HttpResponse("Hello, world!")
+# def home_page_view(request):
+#     return HttpResponse("Hello, world!")
+
+
+class HomePageView(TemplateView):
+    template_name: str = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name: str = "about.html"
